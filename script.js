539e98073b42c65f549628ae7974d7e640ad7b2c
mtkramer <kramerm1@mail.usf.edu>
2020-05-13 09:41:31 -0400

    completed without bonuses

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// TODO: add code here
+window.addEventListener('DOMContentLoaded', () => {
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then((response) => {
+        response.json().then((json) => {
+            let list = json.map((person) => {
+                return `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${person.firstName} ${person.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${person.hoursInSpace}</li>
+                            <li>Active: ${person.active}</li>
+                            <li>Skills: ${person.skills.join(', ')}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${person.picture}"></img>
+                </div>
+                `;
+            });
+            id('container').innerHTML = list;
+        });
+    });
+
+});
+
+function id(x) { return document.getElementById(x); }
